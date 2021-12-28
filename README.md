84a4474990eeb74acd3b858808f244c4b1d3350b
Michael Stapelberg <michael@stapelberg.de>
2021-12-28 10:10:34 +0100

    split rsyncprom parser into separate rsyncparse package

---
+++ README.md
@@ -0,0 +1,8 @@
+# rsyncparse
+
+[![Go Reference](https://pkg.go.dev/badge/github.com/stapelberg/rsyncparse.svg)](https://pkg.go.dev/github.com/stapelberg/rsyncparse)
+
+An rsync output parser.
+
+See also https://github.com/stapelberg/rsyncprom for a wrapper program which
+pushes the extracted transfer details to a Prometheus push gateway.
---
+++ rsyncparse.go
@@ -0,0 +1,94 @@
+// Package rsyncparse implements a parser that extracts transfer details from
+// rsync standard output output.
+//
+// See also https://github.com/stapelberg/rsyncprom for a wrapper program which
+// pushes the extracted transfer details to a Prometheus push gateway.
+//
+// Rsync Requirements
+//
+// Start rsync with --verbose (-v) or --stats to enable printing transfer
+// totals.
+//
+// Do not use the --human-readable (-h) flag in your rsync invocation, otherwise
+// rsyncprom cannot parse the output!
+//
+// Run rsync in the C.UTF-8 locale to prevent rsync from localizing decimal
+// separators and fractional points in big numbers.
+package rsyncparse
+
+import (
+	"bufio"
+	"io"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// Stats contains all data found in rsync output.
+type Stats struct {
+	Found bool
+
+	TotalWritten int64
+	TotalRead    int64
+	BytesPerSec  float64
+	TotalSize    int64
+}
+
+// Speedup calculates the speed-up of using rsync over copying the data as-is.
+func (p *Stats) Speedup() float64 {
+	return float64(p.TotalSize / (p.TotalWritten + p.TotalRead))
+}
+
+var (
+	statsTransferRe = regexp.MustCompile(`^sent ([0-9,]+) bytes  received ([0-9,]+) bytes  ([0-9,.]+) bytes/sec$`)
+
+	statsSizeRe = regexp.MustCompile(`^total size is ([0-9,]+)  speedup is ([0-9,.]+)$`)
+)
+
+// Parse reads from the specified io.Reader and scans individual lines. rsync
+// transfer totals are extracted when found, and returned in the Stats struct.
+func Parse(r io.Reader) (*Stats, error) {
+	p := &Stats{}
+	scan := bufio.NewScanner(r)
+	for scan.Scan() {
+		line := scan.Text()
+		// log.Printf("rsync output line: %q", line)
+		if strings.HasPrefix(line, "sent ") {
+			// e.g.:
+			// sent 1,590 bytes  received 18 bytes  3,216.00 bytes/sec
+			// total size is 1,188,046  speedup is 738.83
+			matches := statsTransferRe.FindStringSubmatch(line)
+			p.Found = true
+			// parse rsync do_big_num(int64 num) output
+			// parse 1[,.]192[,.]097 bytes
+			var err error
+			p.TotalWritten, err = strconv.ParseInt(strings.ReplaceAll(matches[1], ",", ""), 0, 64)
+			if err != nil {
+				return nil, err
+			}
+			p.TotalRead, err = strconv.ParseInt(strings.ReplaceAll(matches[2], ",", ""), 0, 64)
+			if err != nil {
+				return nil, err
+			}
+			p.BytesPerSec, err = strconv.ParseFloat(strings.ReplaceAll(matches[3], ",", ""), 64)
+			if err != nil {
+				return nil, err
+			}
+		} else if strings.HasPrefix(line, "total size is ") {
+			matches := statsSizeRe.FindStringSubmatch(line)
+			p.Found = true
+			var err error
+			p.TotalSize, err = strconv.ParseInt(strings.ReplaceAll(matches[1], ",", ""), 0, 64)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	if err := scan.Err(); err != nil {
+		if err == io.EOF {
+			return p, nil
+		}
+		return nil, err
+	}
+	return p, nil
+}
